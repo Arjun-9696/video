7ad370e819b0729aa3f6fe3ffa9bd9222d6122aa
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-11-02 09:55:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video preload="yes" autoplay src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4" type="video/mp4" width="50%" height="50%" controls></video>
       </header>
     </div>
   );
